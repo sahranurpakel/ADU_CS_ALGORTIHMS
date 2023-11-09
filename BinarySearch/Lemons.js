b643c33014759ed2be34b70080534b5fb6664617
sahranurpakel <sahranurpakelx@gmail.com>
2023-11-10 01:32:05 +0300

    Lemons js

---
+++ BinarySearch/Lemons.js
@@ -0,0 +1,21 @@
+let lemons = (N, M, S) => {
+  let medium = 1;
+  let count = 0;
+  while (medium <= N) {
+    medium = Math.floor((medium + N) / 2);
+    medium = medium + 1;
+    count++;
+  }
+  return count * S + (N - 1) * M;
+};
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (input) => {
+  const [N, M, S] = input.split(" ").map(Number);
+  console.log(lemons(N, M, S));
+  rl.close();
+});
