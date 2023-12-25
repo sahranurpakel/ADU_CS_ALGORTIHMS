97ce95a490284a0b07e127940463db30bdb3baff
Defne Turğut <defneew@outlook.com>
2023-12-25 22:45:44 +0300

    firstMissingPositive

---
+++ .history/ThirdGroup/FirstMissingPositive_20231225223516.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    // Dizi sıralanır
+    nums.sort((a, b) => a - b);
+
+    // Eksik olan en küçük pozitif tamsayıyı bulmak için dizi üzerinde dolaşılır
+    let missing = 1;
+    for (const num of nums) {
+        if (num === missing) {
+            missing++;
+        } else if (num > missing) {
+            // Eğer num, eksik sayıdan büyükse bulunmuştur, döngüyü sonlandırabiliriz
+            break;
+        }
+    }
+
+    return missing;
+};
+
+// Örnek kullanım
+const nums = [3, 4, -1, 1];
+const result = firstMissingPositive(nums);
+console.log(result); // Çıktı: 2
